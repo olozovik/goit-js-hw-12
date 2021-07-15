2c22bf9f8e34f56aec90912171b0420058eec1cb
Oleg Lozovik <oldisc3@gmail.com>
2021-07-15 16:06:42 +0300

    added styles

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix';
 import fetchCountries from './js/fetchCountries';
 import countryTpl from './templates/countryInfo';
 import countryListTpl from './templates/countryList';
 import './sass/main.scss';
 
-const DEBOUNCE_DELAY = 300;
+const DEBOUNCE_DELAY = 500;
 
 const refs = {
   input: document.querySelector('#search-box'),
   countryList: document.querySelector('.country-list'),
   countryInfo: document.querySelector('.country-info'),
 };
+
+Notify.init({
+  position: 'center-top',
+  distance: '20px',
+  showOnlyTheLastOne: true,
+  fontSize: '20px',
+  width: '400px',
+});
 
 refs.input.addEventListener('input', debounce(searchCountry, DEBOUNCE_DELAY));
 
@@ -41,12 +50,12 @@ function searchCountry(event) {
     .then(data => {
       if (data.length > 10) {
         clearDataRander();
-        console.log('Too many matches found. Please enter a more specific name.');
+        Notify.info('Too many matches found. Please enter a more specific name.');
       }
     })
     .catch(error => {
       clearDataRander();
-      console.log('Oops, there is no country with that name');
+      Notify.failure('Oops, there is no country with that name');
     });
 }
 
